6276e8672a80e5812be21cb583c4614a7ac4e8cc
Precious Ogwo <chidinma.ijeom@gmail.com>
2025-07-30 12:59:48 +0000

    added the podcast component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
-import Podcast from "@/components/Podcast";
+
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
       <About />
-      
     </main>
   );
 }
