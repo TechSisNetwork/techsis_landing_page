145aad56fb8123b0199bb59b51b489a0bb0567c2
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-07-31 05:58:48 +0100

    fix: hide over flow on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="w-screen overflow-hidden">
       <Hero />
       <About />
     </main>
