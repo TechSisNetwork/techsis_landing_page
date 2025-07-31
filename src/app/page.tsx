f149d6daa15db106c35b1ce4320faaec4790449e
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-07-31 07:04:05 +0100

    fix:  align section with design, add link to listen now, add id and add section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Podcast from "@/components/Podcast";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="">
       <Hero />
       <About />
+      <Podcast/>
     </main>
   );
 }
--- src/components/Podcast.tsx
+++ src/components/Podcast.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image';
 import PodcastCover from '../../public/podcast.png';
 import Button from './Button';
 import Container from './Container';
+import Link from 'next/link';
 
 const Podcast = () => {
   return (
-    <section className="w-full py-20 md:py-34 bg-[#FFE1E817]">
-      <Container className="grid lg:grid-cols-2 items-center gap-[38.11px]">
+    <section id='podcast' className="w-full py-20 md:py-34">
+      <Container className="flex flex-col lg:flex-row items-center gap-10 lg:gap-[106px]">
         {/* Left - Podcast Cover Image */}
         <div className="w-full max-w-md rounded-2xl overflow-hidden mx-auto lg:mx-0">
           <Image
@@ -19,25 +20,25 @@ const Podcast = () => {
         </div>
 
         {/* Right - Text and Button */}
-        <div className="flex-1 flex flex-col  items-center lg:items-start gap-10 lg:gap-[50px]">
-          <div className="flex flex-col  gap-4 lg:gap-5 text-center lg:text-start">
+
+        <div className="flex flex-col  gap-4 lg:gap-5 items-center lg:items-start text-center lg:text-start">
           <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-[#2B2B2B] leading-tight">
             Bold Voices in Tech<br />and Leadership!
           </h2>
           <p className="text-[#606060]  text-base lg:text-xl">
-       Keep up with everything tech and women empowerment with<br />
-      <span>
-        Ofure on <span className="text-primary font-semibold">'UNSCRIPTED AND UNCHARTED'</span> podcast.
-      </span>
-</p>
+            Keep up with everything tech and women empowerment with<br />
+            <span>
+              Ofure on <span className="text-primary font-semibold">&apos;UNSCRIPTED AND UNCHARTED&apos;</span> podcast.
+            </span>
+          </p>
 
-
-          <Button
-            text="Listen Now"
-            variant="primary"
-            className="!bg-[#404040] hover:!bg-[#303030] text-white !px-[43px] lg:!px-[50px]"
-          />
-        </div>
+          <Link href={'https://open.spotify.com/show/3CwxCU1CPvYlVQWiHrtkog'} target='blank'>
+            <Button
+              text="Listen Now"
+              variant="primary"
+              className="!bg-[#404040] hover:!bg-[#303030] text-white !px-[43px] lg:!px-[50px]"
+            />
+          </Link>
         </div>
       </Container>
     </section>
